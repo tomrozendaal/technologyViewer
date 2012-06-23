f8b831fe61c6ee7fd5b10da7e8097d1389f69ebb
Tom Rozendaal <tomrozendaal5434@gmail.com>
2012-06-23 19:46:05 +0200

    Rising and Declining technologies

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -10,6 +10,7 @@ class PageController < ApplicationController
 
 	def programmingLanguages
 		@csv_file = 'data/latest_pl_aspect_data.csv'
+		@history_file = 'data/history_pl_aspect_data.csv'
 		@current_path = programmingLanguages_path
 		@title = 'Programming Languages'
 
@@ -29,7 +30,8 @@ class PageController < ApplicationController
 					:sentiment => Integer(csv_obj['sentiment']),
 					:newsvalue => Integer(csv_obj['newsworthiness']),
 					:total => Integer(csv_obj['total']),
-					:category => csv_obj['category']
+					:category => csv_obj['category'],
+					:technology => csv_obj['technology']
 				}
 				
 			@total_total = @total_total + Integer(csv_obj['total'])
@@ -38,12 +40,18 @@ class PageController < ApplicationController
 			@total_knowledge = @total_knowledge + Integer(csv_obj['knowledge'])
 			@total_newsvalue = @total_newsvalue + Integer(csv_obj['adoption'])
 			@total_sentiment = @total_sentiment + Integer(csv_obj['sentiment'])
-
 		end
+		if @total_sentiment <= 0
+			@total_sentiment = 50
+		end
+
+		@rising_tech = get_rising(@history_file, @top_rated)
+		@declining_tech = get_declining(@history_file, @top_rated)
 	end
 
 	def webFrameworks
 		@csv_file = 'data/latest_wf_aspect_data.csv'
+		@history_file = 'data/history_wf_aspect_data.csv'
 		@current_path = webFrameworks_path
 		@title = 'Web Frameworks'
 
@@ -63,7 +71,8 @@ class PageController < ApplicationController
 					:sentiment => Integer(csv_obj['sentiment']),
 					:newsvalue => Integer(csv_obj['newsworthiness']),
 					:total => Integer(csv_obj['total']),
-					:category => csv_obj['category']
+					:category => csv_obj['category'],
+					:technology => csv_obj['technology']
 				}
 				
 			@total_total = @total_total + Integer(csv_obj['total'])
@@ -76,10 +85,14 @@ class PageController < ApplicationController
 		if @total_sentiment <= 0
 			@total_sentiment = 50
 		end
+
+		@rising_tech = get_rising(@history_file, @top_rated)
+		@declining_tech = get_declining(@history_file, @top_rated)
 	end
 
 	def contentManagementSystems
 		@csv_file = 'data/latest_cms_aspect_data.csv'
+		@history_file = 'data/history_cms_aspect_data.csv'
 		@current_path = contentManagementSystems_path
 		@title = 'Content Management Systems'
 
@@ -99,7 +112,8 @@ class PageController < ApplicationController
 					:sentiment => Integer(csv_obj['sentiment']),
 					:newsvalue => Integer(csv_obj['newsworthiness']),
 					:total => Integer(csv_obj['total']),
-					:category => csv_obj['category']
+					:category => csv_obj['category'],
+					:technology => csv_obj['technology']
 				}
 				
 			@total_total = @total_total + Integer(csv_obj['total'])
@@ -108,8 +122,13 @@ class PageController < ApplicationController
 			@total_knowledge = @total_knowledge + Integer(csv_obj['knowledge'])
 			@total_newsvalue = @total_newsvalue + Integer(csv_obj['adoption'])
 			@total_sentiment = @total_sentiment + Integer(csv_obj['sentiment'])
-
 		end
+		if @total_sentiment <= 0
+			@total_sentiment = 50
+		end
+
+		@rising_tech = get_rising(@history_file, @top_rated)
+		@declining_tech = get_declining(@history_file, @top_rated)
 	end
 
 	def technology
@@ -132,7 +151,17 @@ class PageController < ApplicationController
 				@tech_data['answered_percentage'] = ts csv_obj['answered_percentage']
 
 				# Sentiment
-				@tech_data['ratio'] = ts csv_obj['positive_ratio']
+				if csv_obj['positive_ratio'].to_i < 0
+					ratio = csv_obj['positive_ratio'].gsub!('-','')
+					@tech_data['ratio'] = "1:#{ratio}"
+				elsif csv_obj['positive_ratio'].to_i == 0
+					@tech_data['ratio'] = "0:0"
+				else
+					@tech_data['ratio'] = "#{csv_obj['positive_ratio']}:1"
+				end
+				@tech_data['positive_mes'] = ts csv_obj['positive_messages']
+				@tech_data['neutral_mes'] = ts csv_obj['neutral_messages']
+				@tech_data['negative_mes'] = ts csv_obj['negative_messages']
 
 				# Newsworthiness
 				@tech_data['actuality'] = ts csv_obj['newsworthiness']
@@ -186,6 +215,59 @@ class PageController < ApplicationController
 			redirect_to "/404/#{query_category}/technology/#{query}"
 		end
 	end
+
+	def get_rising(file, latest)
+		rising_tech = Hash.new
+		FasterCSV.foreach("public/#{file}", :headers => true) do |history_tech|
+			latest.each_with_index do |latest_tech, index|
+				if latest_tech[1][:technology] == history_tech['technology']
+					if latest_tech[1][:total].to_i > history_tech['total'].to_i 
+						rising_tech["#{latest_tech[1][:technology]}".to_sym] = 
+						{
+							:adoption => Integer(latest_tech[1][:adoption]), 
+							:evolution => Integer(latest_tech[1][:evolution]), 
+							:knowledge => Integer(latest_tech[1][:knowledge]), 
+							:sentiment => Integer(latest_tech[1][:sentiment]),
+							:newsvalue => Integer(latest_tech[1][:newsvalue]),
+							:total => Integer(latest_tech[1][:total]),
+							:total_old => Integer(history_tech['total']),
+							:category => latest_tech[1][:category],
+							:technology => latest_tech[1][:technology],
+							:increase => (((latest_tech[1][:total].to_f - history_tech['total'].to_f) / latest_tech[1][:total].to_f) * 100).ceil
+						}
+					end
+				end
+			end
+		end
+		return rising_tech
+	end
+
+	def get_declining(file, latest)
+		declining_tech = Hash.new
+		FasterCSV.foreach("public/#{file}", :headers => true) do |history_tech|
+			latest.each_with_index do |latest_tech, index|
+				if latest_tech[1][:technology] == history_tech['technology']
+					if latest_tech[1][:total].to_i < history_tech['total'].to_i 
+						declining_tech["#{latest_tech[1][:technology]}".to_sym] = 
+						{
+							:adoption => Integer(latest_tech[1][:adoption]), 
+							:evolution => Integer(latest_tech[1][:evolution]), 
+							:knowledge => Integer(latest_tech[1][:knowledge]), 
+							:sentiment => Integer(latest_tech[1][:sentiment]),
+							:newsvalue => Integer(latest_tech[1][:newsvalue]),
+							:total => Integer(latest_tech[1][:total]),
+							:total_old => Integer(history_tech['total']),
+							:category => latest_tech[1][:category],
+							:technology => latest_tech[1][:technology],
+							:decrease => (((history_tech['total'].to_f - latest_tech[1][:total].to_f) / history_tech['total'].to_f) * 100).ceil
+						}
+					end
+				end
+			end
+		end
+		return declining_tech
+	end
+
 
 	def full_category(category)
 		if category == "wf"
